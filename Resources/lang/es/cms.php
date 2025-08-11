c06032fcd9ee3f0cf8a71a27338986eed6398e7e
Cristian Marin <marin101998@gmail.com>
2025-08-11 12:41:27 -0500

    + v8.x | 11-08-25 view with trasnlates

---
+++ Resources/lang/es/cms.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+  "sidebar" => [
+    "adminGroup" => "Certificados",
+    "adminCustomers" => "Clientes",
+    "adminCompanies" => "Empresas",
+    "adminTrainingTypes" => "Tipos de entrenamiento",
+    "adminTrainers" => "Entrenadores",
+    "adminCourses" => "Cursos",
+    "adminCertificates" => "Certificados"
+  ],
+  "form" => [
+    "idDocument" => "Documento de Identidad",
+    "nit" => "NIT",
+    "businessName" => "Nombre de la empresa",
+    "fullName" => "Nombre Completo",
+    "position" => "Posición",
+    "standardDuration" => "Duración estándar",
+    "trainer" => "Entrenador",
+    "trainingType" => "Tipo de Entrenador",
+    "customer" => "Cliente",
+    "company" => "Empresa",
+    "course" => "Curso"
+  ],
+  "message" => [
+    "invalidNIT" => "El NIT no es válido."
+  ],
+  "newCustomer" => "Nuevo Cliente",
+  "updateCustomer" => "Actualizar Cliente",
+  "newCompany" => "Nueva Empresa",
+  "updateCompany" => "Actualizar Empresa",
+  "newTrainingtype" => "Nuevo Tipo de entrenamiento",
+  "updateTrainingtype" => "Actualizar Tipo de entrenamiento",
+  "newTrainer" => "Nuevo Entrenador",
+  "updateTrainer" => "Actualizar Entrenador",
+  "newCourse" => "Nuevo Curso",
+  "updateCourse" => "Actualizar Curso",
+  "newCertificate" => "Nuevo Certificado",
+  "updateCertificate" => "Actualizar Certificado"
+];
