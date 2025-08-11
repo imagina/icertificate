38bac3151a838ccd6bd49ce6428bb0291dc846d4
Cristian Marin <marin101998@gmail.com>
2025-08-11 10:51:48 -0500

    + v8.x | 11-08-25 view with trasnlates

---
+++ Resources/lang/en/pdf.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+  'labelGroupExternalPages'=> 'PDF Certificate',
+  'settings' => [
+    'logoPrimary' => 'Primary Logo',
+    'logoSecondary' => 'Secondary Logo',
+    'logoThird' => 'Third Logo',
+    'logoBackground' => 'Background Logo',
+    'logoSignature' => 'Signature Logo',
+    'nit' => 'NIT',
+    'textFooter' => 'Footer Text',
+    'textFloatRight' => 'Text on the right side',
+    'peopleSignatureName' => 'Name of the person in charge',
+    'peopleSignaturePosition' => 'Position of the person',
+  ],
+  'filePdf' => [
+    'certificateThat' => 'Certifies that',
+    'typeDocument' => 'Document Type',
+    'participation' => 'Participated in the :type training of :hours hours held on :date',
+    'dateExpedition' => 'Date of issue',
+    'instructor' => 'INSTRUCTOR:',
+  ],
+];
